1303ecbe3ecd4470efd2ffe4e6ea268bbe0b653b
Sai Mitheran <49593646+smj007@users.noreply.github.com>
2021-02-27 01:23:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ print(opt)
 
 
 def main():
-    train_data = pickle.load(open('./datasets/' + opt.dataset + '/train.txt', 'rb'))
+    train_data = pickle.load(open('datasets/yoochoose1_64/train.txt', 'rb'))
     if opt.validation:
         train_data, valid_data = split_validation(train_data, opt.valid_portion)
         test_data = valid_data
     else:
-        test_data = pickle.load(open('./datasets/' + opt.dataset + '/test.txt', 'rb'))
+        test_data = pickle.load(open('datasets/yoochoose1_64/test.txt', 'rb'))
 
     train_data = Data(train_data, shuffle=True)
     test_data = Data(test_data, shuffle=False)
