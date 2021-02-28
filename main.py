c8af4a505eacf489ebca49ce418fb1f7c7d63464
Surya Kant Sahu <surya.oju@gmail.com>
2021-02-28 22:07:56 +0530

    Added tensorboard logging

--- main.py
+++ main.py
@@ -11,27 +11,30 @@ import pickle
 import time
 from utils import  Data, split_validation
 from model import *
+from torch.utils.tensorboard import SummaryWriter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='sample', help='dataset name: diginetica/yoochoose1_64')
 parser.add_argument('--batchSize', type=int, default=100, help='input batch size')
-parser.add_argument('--hiddenSize', type=int, default=96, help='hidden state size')
-parser.add_argument('--nhead', type=int, default=8, help='the number of heads of multi-head attention')
-parser.add_argument('--layer', type=int, default=1, help='number of SAN layers')
+parser.add_argument('--hiddenSize', type=int, default=128, help='hidden state size')
+parser.add_argument('--nhead', type=int, default=1, help='the number of heads of multi-head attention')
+parser.add_argument('--layer', type=int, default=4, help='number of SAN layers')
 parser.add_argument('--feedforward', type=int, default=4, help='the multipler of hidden state size')
-parser.add_argument('--epoch', type=int, default=12, help='the number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.001, help='learning rate')  # [0.001, 0.0005, 0.0001]
+parser.add_argument('--epoch', type=int, default=100, help='the number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate')  # [0.001, 0.0005, 0.0001]
 parser.add_argument('--lr_dc', type=float, default=0.1, help='learning rate decay rate')
-parser.add_argument('--lr_dc_step', type=int, default=3, help='the number of steps after which the learning rate decay')
+parser.add_argument('--lr_dc_step', type=int, default=5, help='the number of steps after which the learning rate decay')
 parser.add_argument('--l2', type=float, default=1e-5, help='l2 penalty')  # [0.001, 0.0005, 0.0001, 0.00005, 0.00001]
-parser.add_argument('--patience', type=int, default=10, help='the number of epoch to wait before early stop ')
+parser.add_argument('--patience', type=int, default=100, help='the number of epoch to wait before early stop ')
 parser.add_argument('--validation', action='store_true', help='validation')
 parser.add_argument('--valid_portion', type=float, default=0.1, help='split the portion of training set as validation set')
 opt = parser.parse_args()
 print(opt)
 
+writer = SummaryWriter("./logs/")
 
 def main():
+    step = 0
     train_data = pickle.load(open('datasets/yoochoose1_64/train.txt', 'rb'))
     if opt.validation:
         train_data, valid_data = split_validation(train_data, opt.valid_portion)
@@ -56,7 +59,7 @@ def main():
     for epoch in range(opt.epoch):
         print('-------------------------------------------------------')
         print('epoch: ', epoch)
-        hit, mrr = train_test(model, train_data, test_data)
+        hit, mrr, step = train_test(model, train_data, test_data, writer, step)
         flag = 0
         if hit >= best_result[0]:
             best_result[0] = hit
@@ -66,8 +69,10 @@ def main():
             best_result[1] = mrr
             best_epoch[1] = epoch
             flag = 1
+        if flag:
+            torch.save(model, f"./checkpoints/{opt.dataset}_val_mmr_{best_result[1]}_ep{epoch}.pth")
         print('Best Result:')
-        print('\tRecall@20:\t%.4f\tMMR@20:\t%.4f\tEpoch:\t%d,\t%d'% (best_result[0], best_result[1], best_epoch[0], best_epoch[1]))
+        print('\tRecall@20: %.4f\tMMR@20: %.4f\tEpoch:\t%d,\t%d'% (best_result[0], best_result[1], best_epoch[0], best_epoch[1]))
         bad_counter += 1 - flag
         if bad_counter >= opt.patience:
             break
--- model.py
+++ model.py
@@ -12,64 +12,12 @@ import torch
 from torch import nn
 from torch.nn import Module, Parameter
 import torch.nn.functional as F
-# from torch.nn import TransformerEncoder
-# from torch.nn import TransformerEncoderLayer
 from nfnets.agc import AGC
-from conformer import ConformerEncoder
+from delight import DeLighTTransformer, default_arg
 
-
-# class Conformer(nn.Module):
-#     def __init__(self, dim, depth,
-#                 heads, dim_head,
-#                 dropout, kernel_size=31,
-#                 causal=False):
-#         self.layers = nn.ModuleList([])
-
-#         for _ in range(depth):
-#             self.layers.append(ConformerBlock())
-    
-#     def forward(self, x):
-#         for layer in self.layers:
-#             x = layer(x) + x
-
-#         return x
-
-
-# class ConformerEncoder(nn.Module):
-#     def __init__(self, *, num_tokens, num_classes, dim, depth, heads, dim_head=64, dropout=0., emb_dropout=0., kernel_size=31, causal=False):
-#         super().__init__()
-
-#         self.item_embed = nn.Embedding(num_tokens, dim)
-#         self.pos_emb = FixedPositionalEmbedding(dim)
-#         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-#         self.dropout = nn.Dropout(emb_dropout)
-
-#         self.transformer = Conformer(
-#             dim, depth, heads, dim_head, dropout, kernel_size, causal)
-
-#         self.mlp = nn.Sequential(
-#                 nn.LayerNorm(dim),
-#                 nn.Linear(dim, dim // 2),
-#                 nn.ELU(inplace=True),
-#                 nn.Linear(dim // 2, dim),
-#             )
-
-#     def forward(self, x, mask=None):
-
-#         b, n = x.shape
-#         pos_emb = self.pos_emb(x)
-#         x = pos_emb + self.item_embed(x)
-    
-
-#         cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
-#         x = torch.cat((cls_tokens, x), dim=1)
-#         x = self.dropout(x)
-
-#         x = self.transformer(x, mask)
-#         x = x.mean(dim=1)
-#         decoder_out = self.mlp(x)
-#         return decoder_out
-
+default_arg.delight_enc_max_depth = 8
+default_arg.delight_enc_min_depth = 2
+default_arg.delight_enc_width_mult = 2.0
 
 
 class SelfAttentionNetwork(Module):
@@ -78,25 +26,15 @@ class SelfAttentionNetwork(Module):
         self.hidden_size = opt.hiddenSize
         self.n_node = n_node
         self.batch_size = opt.batchSize
-        # self.embedding = nn.Embedding(self.n_node, self.hidden_size)
-        # self.transformerEncoderLayer = TransformerEncoderLayer(d_model=self.hidden_size, nhead=opt.nhead,dim_feedforward=self.hidden_size * opt.feedforward)
-        # self.transformerEncoder = TransformerEncoder(self.transformerEncoderLayer, opt.layer)
-        # print(self.n_node)
-        self.transformerEncoder = ConformerEncoder(
-            input_dim = self.n_node,
-            encoder_dim = 128,
-            num_layers = opt.layer, 
-            num_attention_heads = opt.nhead,
-            input_dropout_p = 0.1,
-            feed_forward_dropout_p = 0.1,
-            attention_dropout_p = 0.1,
-            conv_dropout_p = 0.1,
 
-        )
-        # self.final_linear = nn.Linear(64, 1)
+        default_arg.delight_enc_layers = opt.layer
+        self.transformerEncoder = DeLighTTransformer(opt.hiddenSize, self.n_node, 139, args=default_arg)
+        
         self.loss_function = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr, weight_decay=opt.l2)
         self.AGC_optim = AGC(self.parameters(), self.optimizer)
+        default_arg.delight_enc_layers = 4
+
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=opt.lr_dc_step, gamma=opt.lr_dc)
         self.reset_parameters()
 
@@ -105,29 +43,9 @@ class SelfAttentionNetwork(Module):
         for weight in self.parameters():
             weight.data.uniform_(-stdv, stdv)
 
-    # def compute_scores(self, hidden, mask):
-    #     ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
-    #     b = self.embedding.weight[1:]  # n_nodes x latent_size
-    #     scores = torch.matmul(ht, b.transpose(1, 0))
-    #     return scores
-
-    # def forward(self, inputs, A):
-    #     hidden = self.embedding(inputs)
-    #     hidden = hidden.transpose(0,1).contiguous()
-    #     hidden = self.transformerEncoder(hidden)
-    #     hidden = hidden.transpose(0,1).contiguous()
-    #     return hidden
-
     def forward(self, inputs):
-        # print(inputs.size())
         hidden = self.transformerEncoder(inputs)
-        # hidden = hidden.transpose(0,1).contiguous()
-        # print(hidden.size())
-        hidden = torch.matmul(hidden, self.transformerEncoder.item_embed.weight[1:].transpose(1, 0)) # weight tying
-        # hidden = self.final_linear(hidden)
-        # hidden = torch.squeeze(hidden, -1)
-        # print(hidden.size())
-
+        # print(hidden.shape)
         return hidden
 
 
@@ -153,13 +71,9 @@ def forward(model, i, data):
     # A = trans_to_cuda(torch.Tensor(A).float())
     mask = trans_to_cuda(torch.Tensor(mask).long())
     hidden = model(items)
-    hidden = hidden.mean(dim=1)
-    # get = lambda i: hidden[i][alias_inputs[i]]
-    # seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
     return targets, hidden # model.compute_scores(seq_hidden, mask)
 
-
-def train_test(model, train_data, test_data):    
+def train_test(model, train_data, test_data, writer, step):    
     print('start training: ', datetime.datetime.now())
     model.train()
     total_loss = 0.0
@@ -169,11 +83,15 @@ def train_test(model, train_data, test_data):
         targets, scores = forward(model, i, train_data)
         targets = trans_to_cuda(torch.Tensor(targets).long())
         loss = model.loss_function(scores, targets - 1)
+
+        writer.add_scalar("train/loss", loss, step)
+
         loss.backward()
         model.AGC_optim.step()
         total_loss += loss
         if j % int(len(slices) / 5 + 1) == 0:
             print('[%d/%d] Loss: %.4f' % (j, len(slices), loss.item()))
+        step += 1
     print('\tLoss:\t%.3f' % total_loss)
 
     print('start predicting: ', datetime.datetime.now())
@@ -193,4 +111,6 @@ def train_test(model, train_data, test_data):
     hit = np.mean(hit) * 100
     mrr = np.mean(mrr) * 100
     model.scheduler.step()
-    return hit, mrr
+    writer.add_scalar("val/recall", hit, step)
+    writer.add_scalar("val/mrr", mrr, step)
+    return hit, mrr, step
